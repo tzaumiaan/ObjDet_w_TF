ebbf58cd1049b13abd635bf278b80cc575ac14d3
Min-An Chao <min-an.chao@intel.com>
2019-05-02 09:34:07 +0200

    backup racecar training results

--- export_racecar.sh
+++ export_racecar.sh
@@ -1,7 +1,7 @@
 INPUT_TYPE=image_tensor
 PIPELINE_CONFIG_PATH=config/ssd_mobilenet_v1_racecar.config
-TRAINED_CKPT_PREFIX=train_racecar_0/model.ckpt-1265
-EXPORT_DIR=model_export
+TRAINED_CKPT_PREFIX=train_racecar_0/model.ckpt-63164
+EXPORT_DIR=ssd_mobilenet_racecar_model_63164
 TF_MODEL_ROOT=~/workspace/models/research
 python ${TF_MODEL_ROOT}/object_detection/export_inference_graph.py \
     --input_type=${INPUT_TYPE} \
